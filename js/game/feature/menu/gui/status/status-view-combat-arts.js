45c7e7cec1985556f1b8ffed5316493d4a30e710
ac2pic <ac2pic@gmail.com>
2020-03-30 17:01:58 -0500

    Added StatusViewCombatArt file

---
+++ js/game/feature/menu/gui/status/status-view-combat-arts.js
@@ -0,0 +1,41 @@
+ig.module("game.feature.ui-replacer.menu.gui.status.status-view-combat-arts").requires("game.feature.menu.gui.status.status-view-combat-arts").defines(function() {
+    function onPlayerModelChanged(model, event) {
+        if (model === sc.model.player) {
+            if (event === sc.PLAYER_MSG.CONFIG_CHANGED) {
+                const playerName = sc.model.player.name;
+                if (customPlayerMenus.has(playerName)) {
+                    this.setConfig(customPlayerMenus.get(playerName));
+                } else {
+                    this.setConfig(null);
+                }
+                
+            }
+        }
+    }
+
+    sc.StatusViewCombatArtsEntry.inject({
+        config: null,
+        init: function() {
+            this.parent(...arguments);
+            this.modelChanged(sc.model.player, sc.PLAYER_MSG.CONFIG_CHANGED);
+        },
+        onAttach: function() {
+            addPlayerObserver(this);
+        },
+        onDetach: function() {
+            removePlayerObserver(this);
+        },
+        updateDrawables: function() {
+            const config = this.config;
+
+            let circuitIconGfx = this.skillIcons;
+            if (config !== null) {
+                if (config.circuitIconGfx) {
+                    circuitIconGfx = config.circuitIconGfx;
+                }
+            }
+            this.icon.image = circuitIconGfx;
+        },
+        modelChanged: onPlayerModelChanged
+    });
+});
--- postload.js
+++ postload.js
@@ -1,3 +1,5 @@
 import "./js/game/feature/menu/gui/main-menu.js";
 
 import "./js/game/feature/menu/gui/circuit-icons.js";
+
+import "./js/game/feature/menu/gui/status/status-view-combat-arts.js";
