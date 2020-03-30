33fde31428f5d5b0bab51e92f650184c64604193
ac2pic <ac2pic@gmail.com>
2020-03-30 12:20:38 -0500

    Added basic mod stuff

---
+++ plugin.js
@@ -0,0 +1,18 @@
+export default class PartyPlayer extends Plugin {
+	constructor(mod) {
+		super();
+		this.mod = mod;
+	}
+
+	async preload() {
+
+	}
+
+	async postload() {
+
+	}
+
+	async prestart() {
+
+	}
+}
