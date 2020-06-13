c1699613e958e3ae6ca1ff590db519f851a9228c
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-06-13 16:01:09 +0300

    reformat

--- extendable-severed-heads.js
+++ extendable-severed-heads.js
@@ -1,149 +1,139 @@
 ig.module('extendable-severed-heads')
-	.requires('impact.base.impact', 'game.feature.player.player-config')
-	.defines(() => {
-		const HEAD_INDEXES_FILE_PATH = 'data/players/headIdx.json';
+    .requires('impact.base.impact', 'game.feature.player.player-config')
+    .defines(() => {
+        const HEAD_INDEXES_FILE_PATH = 'data/players/headIdx.json';
 
-		sc.ExtendableSeveredHeads = ig.JsonLoadable.extend({
-			cacheType: 'ExtendableSeveredHeads',
-			indexes: {},
-			customIds: [],
-			baseImage: null,
-			customImages: [],
-			loadCollector: null,
+        sc.ExtendableSeveredHeads = ig.JsonLoadable.extend({
+            cacheType: 'ExtendableSeveredHeads',
+            indexes: {},
+            customIds: [],
+            baseImage: null,
+            customImages: [],
+            loadCollector: null,
 
-			init() {
-				this.parent(HEAD_INDEXES_FILE_PATH);
-			},
+            init() {
+                this.parent(HEAD_INDEXES_FILE_PATH);
+            },
 
-			getCacheKey() {
-				return HEAD_INDEXES_FILE_PATH;
-			},
+            getCacheKey() {
+                return HEAD_INDEXES_FILE_PATH;
+            },
 
-			getJsonPath() {
-				return `${ig.root}${this.path}${ig.getCacheSuffix()}`;
-			},
+            getJsonPath() {
+                return `${ig.root}${this.path}${ig.getCacheSuffix()}`;
+            },
 
-			onload(data) {
-				this.loadCollector = new ig.LoadCollector(this);
+            onload(data) {
+                this.loadCollector = new ig.LoadCollector(this);
 
-				this.baseImage = new ig.Image('media/gui/severed-heads.png');
-				for (const head of data.headIdx) {
-					this.customIds.push(head.id);
-					this.customImages.push(new ig.Image(head.img));
-				}
+                this.baseImage = new ig.Image('media/gui/severed-heads.png');
+                for (const head of data.headIdx) {
+                    this.customIds.push(head.id);
+                    this.customImages.push(new ig.Image(head.img));
+                }
 
-				this.loadCollector.finalizeLoadableFetching();
-			},
+                this.loadCollector.finalizeLoadableFetching();
+            },
 
-			onCacheCleared() {
-				this.baseImage.decreaseRef();
-				for (const img of this.customImages) img.decreaseRef();
-			},
+            onCacheCleared() {
+                this.baseImage.decreaseRef();
+                for (const img of this.customImages) img.decreaseRef();
+            },
 
-			onLoadableComplete(success, loadable) {
-				if (!(success && loadable === this.loadCollector)) return;
+            onLoadableComplete(success, loadable) {
+                if (!(success && loadable === this.loadCollector)) return;
 
-				if (this.baseImage.failed) return;
-				for (const img of this.customImages) {
-					if (img.failed) return;
-				}
+                if (this.baseImage.failed) return;
+                for (const img of this.customImages) {
+                    if (img.failed) return;
+                }
 
-				this._assignCustomIndexes();
-				this._patchBaseImage();
-			},
+                this._assignCustomIndexes();
+                this._patchBaseImage();
+            },
 
-			_assignCustomIndexes() {
-				const startIndex = Math.floor(this.baseImage.width / 24);
-				for (let i = 0; i < this.customIds.length; i++) {
-					const id = this.customIds[i];
-					this.indexes[id] = startIndex + i;
-				}
-			},
+            _assignCustomIndexes() {
+                const startIndex = Math.floor(this.baseImage.width / 24);
+                for (let i = 0; i < this.customIds.length; i++) {
+                    const id = this.customIds[i];
+                    this.indexes[id] = startIndex + i;
+                }
+            },
 
-			_patchBaseImage() {
-				const canvas = document.createElement('canvas');
+            _patchBaseImage() {
+                const canvas = document.createElement('canvas');
 
-				const baseImg = this.baseImage;
-				if (baseImg.height !== 24) {
-					throw new Error(
-						"extended-severed-heads: height of the base image isn't 24. please ask the maintainers to fix this mod ASAP!",
-					);
-				}
-				const roundedBaseImageWidth = baseImg.width - (baseImg.width % 24);
+                const baseImg = this.baseImage;
+                if (baseImg.height !== 24) {
+                    throw new Error(
+                        "extended-severed-heads: height of the base image isn't 24. please ask the maintainers to fix this mod ASAP!",
+                    );
+                }
+                const roundedBaseImageWidth = baseImg.width - (baseImg.width % 24);
 
-				canvas.width = roundedBaseImageWidth + this.customImages.length * 24;
-				canvas.height = 24;
+                canvas.width = roundedBaseImageWidth + this.customImages.length * 24;
+                canvas.height = 24;
 
-				const ctx = canvas.getContext('2d');
+                const ctx = canvas.getContext('2d');
 
-				function drawImage(data, srcX, srcY, sizeX, sizeY, destX, destY) {
-					ctx.drawImage(
-						data,
-						srcX,
-						srcY,
-						sizeX,
-						sizeY,
-						destX,
-						destY,
-						sizeX,
-						sizeY,
-					);
-				}
+                function drawImage(data, srcX, srcY, sizeX, sizeY, destX, destY) {
+                    ctx.drawImage(data, srcX, srcY, sizeX, sizeY, destX, destY, sizeX, sizeY);
+                }
 
-				drawImage(baseImg.data, 0, 0, roundedBaseImageWidth, 24, 0, 0);
-				let offset = roundedBaseImageWidth;
-				for (const img of this.customImages) {
-					drawImage(img.data, 0, 0, 24, 24, offset, 0);
-					offset += 24;
-				}
+                drawImage(baseImg.data, 0, 0, roundedBaseImageWidth, 24, 0, 0);
+                let offset = roundedBaseImageWidth;
+                for (const img of this.customImages) {
+                    drawImage(img.data, 0, 0, 24, 24, offset, 0);
+                    offset += 24;
+                }
 
-				this.baseImage.data = canvas;
-			},
-		});
+                this.baseImage.data = canvas;
+            },
+        });
 
-		sc.extendableSeveredHeads = new sc.ExtendableSeveredHeads();
+        sc.extendableSeveredHeads = new sc.ExtendableSeveredHeads();
 
-		sc.PlayerConfig.inject({
-			onload() {
-				let result = this.parent.apply(this, arguments);
+        sc.PlayerConfig.inject({
+            onload() {
+                let result = this.parent.apply(this, arguments);
 
-				sc.extendableSeveredHeads.addLoadListener({
-					onLoadableComplete: (success, loadable) => {
-						if (!success) return;
-						const id = this.character.name;
-						const { indexes } = loadable;
-						if (id in indexes) this.headIdx = indexes[id];
-					},
-				});
+                sc.extendableSeveredHeads.addLoadListener({
+                    onLoadableComplete: (success, loadable) => {
+                        if (!success) return;
+                        const id = this.character.name;
+                        const { indexes } = loadable;
+                        if (id in indexes) this.headIdx = indexes[id];
+                    },
+                });
 
-				return result;
-			},
-		});
-	});
+                return result;
+            },
+        });
+    });
 
 ig.module('extendable-severed-heads.save-slot-gui-party')
-	.requires('extendable-severed-heads', 'game.feature.menu.gui.save.save-misc')
-	.defines(() => {
-		sc.SaveSlotParty.inject({
-			setParty(save) {
-				this.party[0] = sc.party.models[save.player.playerConfig].getHeadIdx();
-				return this.parent.apply(this, arguments);
-			},
-		});
-	});
+    .requires('extendable-severed-heads', 'game.feature.menu.gui.save.save-misc')
+    .defines(() => {
+        sc.SaveSlotParty.inject({
+            setParty(save) {
+                this.party[0] = sc.party.models[save.player.playerConfig].getHeadIdx();
+                return this.parent.apply(this, arguments);
+            },
+        });
+    });
 
 ig.module('extendable-severed-heads.combat-hud')
-	.requires('extendable-severed-heads', 'game.feature.gui.hud.combat-hud')
-	.defines(() => {
-		sc.CombatUpperHud.inject({
-			init() {
-				this.parent();
-				const { pvp } = this.sub;
-				const renderHeads = pvp._renderHeads;
-				pvp._renderHeads = function(_renderer, _x, left, heads) {
-					if (left) heads[0] = sc.model.player.config.headIdx;
-					return renderHeads.apply(this, arguments);
-				};
-			},
-		});
-	});
+    .requires('extendable-severed-heads', 'game.feature.gui.hud.combat-hud')
+    .defines(() => {
+        sc.CombatUpperHud.inject({
+            init() {
+                this.parent();
+                const { pvp } = this.sub;
+                const renderHeads = pvp._renderHeads;
+                pvp._renderHeads = function(_renderer, _x, left, heads) {
+                    if (left) heads[0] = sc.model.player.config.headIdx;
+                    return renderHeads.apply(this, arguments);
+                };
+            },
+        });
+    });
--- menu-ui-replacer.js
+++ menu-ui-replacer.js
@@ -1,266 +1,238 @@
 ig.module('menu-ui-replacer')
-  .requires('impact.base.impact')
-  .defines(() => {
-    const MENU_FILE_PATH = 'data/menu.json';
+    .requires('impact.base.impact')
+    .defines(() => {
+        const MENU_FILE_PATH = 'data/menu.json';
 
-    sc.MenuUiReplacer = ig.JsonLoadable.extend({
-      cacheType: 'MenuUiReplacer',
-      customMenus: null,
-      currentConfig: null,
+        sc.MenuUiReplacer = ig.JsonLoadable.extend({
+            cacheType: 'MenuUiReplacer',
+            customMenus: null,
+            currentConfig: null,
 
-      init() {
-        this.parent(MENU_FILE_PATH);
-        this.customMenus = new Map();
-      },
+            init() {
+                this.parent(MENU_FILE_PATH);
+                this.customMenus = new Map();
+            },
 
-      getCacheKey() {
-        return MENU_FILE_PATH;
-      },
+            getCacheKey() {
+                return MENU_FILE_PATH;
+            },
 
-      getJsonPath() {
-        return `${ig.root}${this.path}${ig.getCacheSuffix()}`;
-      },
+            getJsonPath() {
+                return `${ig.root}${this.path}${ig.getCacheSuffix()}`;
+            },
 
-      onload(data) {
-        for (const menu of data) {
-          menu.gfx = new ig.Image(menu.gfx);
-          if (menu.circuitIconGfx != null) {
-            menu.circuitIconGfx = new ig.Image(menu.circuitIconGfx);
-          }
-          this.customMenus.set(menu.name, menu);
-        }
-      },
+            onload(data) {
+                for (const menu of data) {
+                    menu.gfx = new ig.Image(menu.gfx);
+                    if (menu.circuitIconGfx != null) {
+                        menu.circuitIconGfx = new ig.Image(menu.circuitIconGfx);
+                    }
+                    this.customMenus.set(menu.name, menu);
+                }
+            },
 
-      modelChanged(model, event) {
-        if (
-          model === sc.model.player &&
-          event === sc.PLAYER_MSG.CONFIG_CHANGED
-        ) {
-          // config can be null if it wasn't found
-          this.currentConfig = this.customMenus.get(sc.model.player.name);
-        }
-      },
+            modelChanged(model, event) {
+                if (model === sc.model.player && event === sc.PLAYER_MSG.CONFIG_CHANGED) {
+                    // config can be null if it wasn't found
+                    this.currentConfig = this.customMenus.get(sc.model.player.name);
+                }
+            },
+        });
+
+        sc.menuUiReplacer = new sc.MenuUiReplacer();
     });
-
-    sc.menuUiReplacer = new sc.MenuUiReplacer();
-  });
 
 ig.module('menu-ui-replacer.main-menu')
-  .requires('game.feature.menu.gui.main-menu')
-  .defines(() => {
-    sc.MainMenu.LeaLarge.inject({
-      updateDrawables(renderer) {
-        const config = sc.menuUiReplacer.currentConfig;
-        if (config == null) return this.parent(renderer);
+    .requires('game.feature.menu.gui.main-menu')
+    .defines(() => {
+        sc.MainMenu.LeaLarge.inject({
+            updateDrawables(renderer) {
+                const config = sc.menuUiReplacer.currentConfig;
+                if (config == null) return this.parent(renderer);
 
-        const gfx = config.gfx;
-        const { gfxOffX, gfxOffY, offX, offY, sizeX, sizeY } = config.Large;
-        renderer
-          .addDraw()
-          .setGfx(gfx, gfxOffX, gfxOffY, offX, offY, sizeX, sizeY);
-      },
-    });
+                const gfx = config.gfx;
+                const { gfxOffX, gfxOffY, offX, offY, sizeX, sizeY } = config.Large;
+                renderer.addDraw().setGfx(gfx, gfxOffX, gfxOffY, offX, offY, sizeX, sizeY);
+            },
+        });
 
-    sc.MainMenu.LeaSmall.inject({
-      updateDrawables(renderer) {
-        const config = sc.menuUiReplacer.currentConfig;
-        if (config == null) return this.parent(renderer);
+        sc.MainMenu.LeaSmall.inject({
+            updateDrawables(renderer) {
+                const config = sc.menuUiReplacer.currentConfig;
+                if (config == null) return this.parent(renderer);
 
-        const gfx = config.gfx;
-        const { gfxOffX, gfxOffY, offX, offY, sizeX, sizeY } = config.Small;
-        renderer
-          .addDraw()
-          .setGfx(gfx, gfxOffX, gfxOffY, offX, offY, sizeX, sizeY);
-      },
-    });
+                const gfx = config.gfx;
+                const { gfxOffX, gfxOffY, offX, offY, sizeX, sizeY } = config.Small;
+                renderer.addDraw().setGfx(gfx, gfxOffX, gfxOffY, offX, offY, sizeX, sizeY);
+            },
+        });
 
-    sc.AreaButton.inject({
-      updateDrawables(renderer) {
-        const config = sc.menuUiReplacer.currentConfig;
-        if (config == null) return this.parent(renderer);
+        sc.AreaButton.inject({
+            updateDrawables(renderer) {
+                const config = sc.menuUiReplacer.currentConfig;
+                if (config == null) return this.parent(renderer);
 
-        const { activeArea } = this;
-        this.activeArea = false;
-        this.parent(renderer);
-        this.activeArea = activeArea;
+                const { activeArea } = this;
+                this.activeArea = false;
+                this.parent(renderer);
+                this.activeArea = activeArea;
 
-        if (activeArea) {
-          renderer.addGfx(this.gfx, 1, 2, 304, 440, 3, 3);
+                if (activeArea) {
+                    renderer.addGfx(this.gfx, 1, 2, 304, 440, 3, 3);
 
-          const gfx = config.gfx;
-          const {
-            gfxOffX,
-            gfxOffY,
-            offX,
-            offY,
-            sizeX,
-            sizeY,
-          } = config.AreaButton;
-          renderer.addGfx(gfx, gfxOffX, gfxOffY, offX, offY, sizeX, sizeY);
-        }
-      },
-    });
+                    const gfx = config.gfx;
+                    const { gfxOffX, gfxOffY, offX, offY, sizeX, sizeY } = config.AreaButton;
+                    renderer.addGfx(gfx, gfxOffX, gfxOffY, offX, offY, sizeX, sizeY);
+                }
+            },
+        });
 
-    sc.MapFloorButtonContainer.inject({
-      originalCopy: null,
+        sc.MapFloorButtonContainer.inject({
+            originalCopy: null,
 
-      init(...args) {
-        this.parent(...args);
-        this.leaIconOriginal = this.leaIcon;
-        this._updateLeaIcon();
-      },
+            init(...args) {
+                this.parent(...args);
+                this.leaIconOriginal = this.leaIcon;
+                this._updateLeaIcon();
+            },
 
-      addObservers(...args) {
-        this.parent(...args);
-        sc.Model.addObserver(sc.model.player, this);
-      },
+            addObservers(...args) {
+                this.parent(...args);
+                sc.Model.addObserver(sc.model.player, this);
+            },
 
-      removeObservers(...args) {
-        this.parent(...args);
-        sc.Model.removeObserver(sc.model.player, this);
-      },
+            removeObservers(...args) {
+                this.parent(...args);
+                sc.Model.removeObserver(sc.model.player, this);
+            },
 
-      _updateLeaIcon() {
-        this.leaIcon.doStateTransition('HIDDEN', true);
+            _updateLeaIcon() {
+                this.leaIcon.doStateTransition('HIDDEN', true);
 
-        const config = sc.menuUiReplacer.currentConfig;
-        if (config != null) {
-          const { offX, offY, sizeX, sizeY } = config.MapFloorButtonContainer;
-          this.leaIcon = new ig.ImageGui(config.gfx, offX, offY, sizeX, sizeY);
-          this.leaIcon.hook.transitions = this.leaIconOriginal.hook.transitions;
-        } else {
-          this.leaIcon = this.leaIconOriginal;
-        }
+                const config = sc.menuUiReplacer.currentConfig;
+                if (config != null) {
+                    const { offX, offY, sizeX, sizeY } = config.MapFloorButtonContainer;
+                    this.leaIcon = new ig.ImageGui(config.gfx, offX, offY, sizeX, sizeY);
+                    this.leaIcon.hook.transitions = this.leaIconOriginal.hook.transitions;
+                } else {
+                    this.leaIcon = this.leaIconOriginal;
+                }
 
-        this._createButtons(true);
-      },
+                this._createButtons(true);
+            },
 
-      modelChanged(model, event) {
-        this.parent(model, event);
-        if (
-          model === sc.model.player &&
-          event === sc.PLAYER_MSG.CONFIG_CHANGED
-        ) {
-          this._updateLeaIcon();
-        }
-      },
-    });
+            modelChanged(model, event) {
+                this.parent(model, event);
+                if (model === sc.model.player && event === sc.PLAYER_MSG.CONFIG_CHANGED) {
+                    this._updateLeaIcon();
+                }
+            },
+        });
 
-    function customStatusDrawables(renderer) {
-      sc.MenuPanel.prototype.updateDrawables.apply(this, arguments);
+        function customStatusDrawables(renderer) {
+            sc.MenuPanel.prototype.updateDrawables.apply(this, arguments);
 
-      const config = sc.menuUiReplacer.currentConfig;
-      if (config != null) {
-        const { gfxOffX, gfxOffY, offX, offY, sizeX, sizeY } = config.Head;
-        renderer.addGfx(config.gfx, gfxOffX, gfxOffY, offX, offY, sizeX, sizeY);
-      } else {
-        renderer.addGfx(this.menuGfx, 0, 0, 280, 472, 126, 35);
-      }
+            const config = sc.menuUiReplacer.currentConfig;
+            if (config != null) {
+                const { gfxOffX, gfxOffY, offX, offY, sizeX, sizeY } = config.Head;
+                renderer.addGfx(config.gfx, gfxOffX, gfxOffY, offX, offY, sizeX, sizeY);
+            } else {
+                renderer.addGfx(this.menuGfx, 0, 0, 280, 472, 126, 35);
+            }
 
-      renderer.addGfx(
-        this.statusGfx,
-        64,
-        5,
-        104,
-        32 + sc.model.player.currentElementMode * 24,
-        24,
-        24,
-      );
-    }
-
-    sc.ItemStatusDefault.inject({
-      updateDrawables: customStatusDrawables,
-    });
-    sc.StatusViewMainParameters.inject({
-      updateDrawables: customStatusDrawables,
-    });
-
-    sc.SocialPartyBox.inject({
-      updatePartyLeader() {
-        const oldLeader = this.members[0];
-        oldLeader.hide(true);
-        this.removeChildGui(oldLeader);
-
-        const newLeader = new sc.SocialPartyMember(true, sc.model.player);
-        if (this.isHidden) {
-          newLeader.hide(true);
-        } else {
-          newLeader.show(true);
+            renderer.addGfx(
+                this.statusGfx,
+                64,
+                5,
+                104,
+                32 + sc.model.player.currentElementMode * 24,
+                24,
+                24,
+            );
         }
 
-        this.members[0] = newLeader;
-        this.insertChildGui(newLeader);
-      },
+        sc.ItemStatusDefault.inject({ updateDrawables: customStatusDrawables });
+        sc.StatusViewMainParameters.inject({ updateDrawables: customStatusDrawables });
 
-      show(...args) {
-        this.parent(...args);
-        this.isHidden = false;
-      },
+        sc.SocialPartyBox.inject({
+            updatePartyLeader() {
+                const oldLeader = this.members[0];
+                oldLeader.hide(true);
+                this.removeChildGui(oldLeader);
 
-      hide(...args) {
-        this.parent(...args);
-        this.isHidden = true;
-      },
+                const newLeader = new sc.SocialPartyMember(true, sc.model.player);
+                if (this.isHidden) {
+                    newLeader.hide(true);
+                } else {
+                    newLeader.show(true);
+                }
+
+                this.members[0] = newLeader;
+                this.insertChildGui(newLeader);
+            },
+
+            show(...args) {
+                this.parent(...args);
+                this.isHidden = false;
+            },
+
+            hide(...args) {
+                this.parent(...args);
+                this.isHidden = true;
+            },
+        });
+
+        sc.SocialMenu.inject({
+            addObservers() {
+                this.parent();
+                sc.Model.addObserver(sc.model.player, this);
+            },
+
+            removeObservers() {
+                this.parent();
+                sc.Model.removeObserver(sc.model.player, this);
+            },
+
+            modelChanged(model, event) {
+                this.parent(model, event);
+                if (model === sc.model.player && event === sc.PLAYER_MSG.CONFIG_CHANGED) {
+                    this.party.updatePartyLeader();
+                }
+            },
+        });
     });
-
-    sc.SocialMenu.inject({
-      addObservers() {
-        this.parent();
-        sc.Model.addObserver(sc.model.player, this);
-      },
-
-      removeObservers() {
-        this.parent();
-        sc.Model.removeObserver(sc.model.player, this);
-      },
-
-      modelChanged(model, event) {
-        this.parent(model, event);
-        if (
-          model === sc.model.player &&
-          event === sc.PLAYER_MSG.CONFIG_CHANGED
-        ) {
-          this.party.updatePartyLeader();
-        }
-      },
-    });
-  });
 
 ig.module('menu-ui-replacer.circuit-icons')
-  .requires('game.feature.menu.gui.circuit.circuit-effect-display')
-  .defines(() => {
-    function circuitIconUpdateDrawables(renderer) {
-      const originalIcons = this.icons;
+    .requires('game.feature.menu.gui.circuit.circuit-effect-display')
+    .defines(() => {
+        function circuitIconUpdateDrawables(renderer) {
+            const originalIcons = this.icons;
 
-      const config = sc.menuUiReplacer.currentConfig;
-      if (config != null && config.circuitIconGfx != null) {
-        this.icons = config.circuitIconGfx;
-      }
+            const config = sc.menuUiReplacer.currentConfig;
+            if (config != null && config.circuitIconGfx != null) {
+                this.icons = config.circuitIconGfx;
+            }
 
-      this.parent(renderer);
-      this.icons = originalIcons;
-    }
+            this.parent(renderer);
+            this.icons = originalIcons;
+        }
 
-    sc.CircuitTreeDetail.Node.inject({
-      updateDrawables: circuitIconUpdateDrawables,
+        sc.CircuitTreeDetail.Node.inject({ updateDrawables: circuitIconUpdateDrawables });
+        sc.CircuitSwapBranchesInfoBox.Skill.inject({ updateDrawables: circuitIconUpdateDrawables });
     });
-    sc.CircuitSwapBranchesInfoBox.Skill.inject({
-      updateDrawables: circuitIconUpdateDrawables,
-    });
-  });
 
 ig.module('menu-ui-replacer.status-combat-arts')
-  .requires('game.feature.menu.gui.status.status-view-combat-arts')
-  .defines(() => {
-    sc.StatusViewCombatArtsEntry.inject({
-      updateDrawables(renderer) {
-        const config = sc.menuUiReplacer.currentConfig;
-        this.icon.image =
-          config != null && config.circuitIconGfx != null
-            ? config.circuitIconGfx
-            : this.skillIcons;
+    .requires('game.feature.menu.gui.status.status-view-combat-arts')
+    .defines(() => {
+        sc.StatusViewCombatArtsEntry.inject({
+            updateDrawables(renderer) {
+                const config = sc.menuUiReplacer.currentConfig;
+                this.icon.image =
+                    config != null && config.circuitIconGfx != null
+                        ? config.circuitIconGfx
+                        : this.skillIcons;
 
-        this.parent(renderer);
-      },
+                this.parent(renderer);
+            },
+        });
     });
-  });
