8fa0c560c3d990643d98dfafc9a38a131dcf9e91
ac2pic <ac2pic@gmail.com>
2020-06-13 14:29:51 -0500

    *

---
+++ image/utils.js
@@ -0,0 +1,36 @@
+export class ImageUtils {
+    static async createImage(src) {
+        return new Promise((resolve, reject) => {
+            const img = new Image;
+            img.onload = function () {
+                resolve(img);
+            }
+            img.onerror = function () {
+                reject();
+            }
+            img.src = src;
+        });
+    }
+
+    static async loadImage(src) {
+        return await new Promise((resolve, reject) => {
+            const img = new ig.Image(src);
+            img.addLoadListener({
+                onLoadableComplete: function (loaded, image) {
+                    // replace image
+                    if (loaded) {
+                        debugger;
+                        image.decreaseRef();
+                        if (!image.failed) {
+                            resolve(img.data);
+
+                        } else {
+                            reject();
+                        }
+
+                    }
+                }
+            });
+        });
+    }
+}
